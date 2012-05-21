e6dd03fc53d58ca3dcfe27816bc53105c5a7cbda
Paul Saikko <paul.saikko@cs.helsinki.fi>
2012-05-21 12:20:02 +0300

    added button onClickListener

--- beddit_alarm/gen/com/example/R.java
+++ beddit_alarm/gen/com/example/R.java
@@ -13,10 +13,16 @@ public final class R {
     public static final class drawable {
         public static final int simplebutton=0x7f020000;
     }
+    public static final class id {
+        public static final int alarmTimePicker=0x7f050000;
+        public static final int setAlarmButton=0x7f050001;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
+        public static final int alarm_set=0x7f040001;
+        public static final int alarm_unset=0x7f040002;
         public static final int app_name=0x7f040000;
     }
 }
--- beddit_alarm/src/com/example/MyActivity.java
+++ beddit_alarm/src/com/example/MyActivity.java
@@ -2,6 +2,12 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 public class MyActivity extends Activity
 {
@@ -12,5 +18,30 @@ public class MyActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        AlarmSetButtonClickListener l = new AlarmSetButtonClickListener();
+        ((Button)findViewById(R.id.setAlarmButton)).setOnClickListener(l);
+
+        TimePicker mTimePicker = (TimePicker)this.findViewById(R.id.alarmTimePicker);
+        try {
+            if (((ViewGroup) mTimePicker.getChildAt(0)).getChildCount() > 2)
+                ((ViewGroup) mTimePicker.getChildAt(0)).getChildAt(2).setBackgroundResource(R.drawable.simplebutton);   // AM/PM button
+            for (int j=0; j < mTimePicker.getChildCount(); j++)  {
+                for (int i=0; i<2; i++) {
+                    ((ViewGroup) ((ViewGroup) mTimePicker.getChildAt(0)).getChildAt(i)).getChildAt(0).setBackgroundResource(R.drawable.simplebutton);  // incr time
+                    ((ViewGroup) ((ViewGroup) mTimePicker.getChildAt(0)).getChildAt(i)).getChildAt(1).setBackgroundResource(R.drawable.simplebutton);  // time button
+                    ((ViewGroup) ((ViewGroup) mTimePicker.getChildAt(0)).getChildAt(i)).getChildAt(2).setBackgroundResource(R.drawable.simplebutton);  // decr time
+                }
+            }
+        } catch (Throwable t) {
+            Toast.makeText(this.getApplicationContext(), "error", 1000);
+        }
+    }
+
+    public class AlarmSetButtonClickListener implements OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "!!!!!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
