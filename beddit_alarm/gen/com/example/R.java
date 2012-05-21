0fc0f2818d8bc2c442d1fd7248952865dfb3c502
Paul Saikko <paul.saikko@cs.helsinki.fi>
2012-05-21 10:53:45 +0300

    added custom button

--- beddit_alarm/gen/com/example/R.java
+++ beddit_alarm/gen/com/example/R.java
@@ -10,10 +10,13 @@ package com.example;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int simplebutton=0x7f020000;
+    }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
 }
