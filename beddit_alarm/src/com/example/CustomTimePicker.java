199173e163d17e0902675724fbebc99c50f277ad
Paul Saikko <paul.saikko@cs.helsinki.fi>
2012-05-22 14:14:10 +0300

    fixed time output

--- beddit_alarm/src/com/example/CustomTimePicker.java
+++ beddit_alarm/src/com/example/CustomTimePicker.java
@@ -27,11 +27,8 @@ public class CustomTimePicker extends View {
     final float grabPointOffset = 0.85f;
     final float grabPointSize = 0.1f;
 
-    final double minuteAngle = Math.PI / 30.0;
-    final double hourAngle = Math.PI / 6.0;
-
-    float minHand = 0f;
-    float hourHand = 0f;
+    final double minuteIncrement = Math.PI / 30.0;
+    final double hourIncrement = Math.PI / 6.0;
 
     float radius = 0;
 
@@ -43,18 +40,8 @@ public class CustomTimePicker extends View {
     float midX;
     float midY;
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-
-        super.onSizeChanged(w, h, oldw, oldh);
-        updateSize();
-    }
-
-    private void updateSize() {
-        midX = getWidth() / 2f;
-        midY = getHeight() / 2f;
-        radius = Math.min(getWidth(), getHeight()) / 2f;
-    }
+    int minutes = 0;
+    int hours = 0;
 
     public CustomTimePicker(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -64,6 +51,8 @@ public class CustomTimePicker extends View {
             Log.i("attribute", attrs.getAttributeName(i));
 
         this.size = Integer.parseInt(attrs.getAttributeValue("http://schemas.android.com/apk/res/com.example", "size"));
+        this.minutes = Integer.parseInt(attrs.getAttributeValue("http://schemas.android.com/apk/res/com.example", "minutes"));
+        this.hours = Integer.parseInt(attrs.getAttributeValue("http://schemas.android.com/apk/res/com.example", "hours"));
         updateSize();
     }
 
@@ -81,10 +70,10 @@ public class CustomTimePicker extends View {
         c.drawCircle(midX,midY,radius,clockMinBackground);
         c.drawCircle(midX,midY,radius*0.75f,clockHourBackground);
 
-        float hx = (float)Math.cos(hourHand-Math.PI/2) * radius * 0.75f;
-        float hy = (float)Math.sin(hourHand-Math.PI/2) * radius * 0.75f;
-        float mx = (float)Math.cos(minHand-Math.PI/2) * radius;
-        float my = (float)Math.sin(minHand-Math.PI/2) * radius;
+        float hx = (float)Math.cos(getHourHandAngle() - Math.PI/2) * radius * 0.75f;
+        float hy = (float)Math.sin(getHourHandAngle() - Math.PI/2) * radius * 0.75f;
+        float mx = (float)Math.cos(getMinuteHandAngle() - Math.PI/2) * radius;
+        float my = (float)Math.sin(getMinuteHandAngle() - Math.PI/2) * radius;
 
         minGrabX = midX+mx*grabPointOffset;
         minGrabY = midY+my*grabPointOffset;
@@ -97,10 +86,11 @@ public class CustomTimePicker extends View {
         c.drawCircle(hourGrabX, hourGrabY, radius*grabPointSize, clockHand);
 
         Paint textPaint = new Paint();
-        textPaint.setTextSize(20);
+        textPaint.setTextSize(40);
         textPaint.setAntiAlias(true);
+        textPaint.setColor(Color.CYAN);
 
-        String time =  getHours()+":"+getMinutes();
+        String time = timeToString(hours,minutes);
         float textLength = textPaint.measureText(time);
 
         c.drawText(time,midX-textLength/2,midY,textPaint);
@@ -109,13 +99,34 @@ public class CustomTimePicker extends View {
     boolean hourGrabbed = false;
     boolean minGrabbed = false;
 
+    public String timeToString(int h, int m) {
+        String hours = h < 10 ? "0"+h : Integer.toString(h);
+        String minutes = m < 10 ? "0"+m : Integer.toString(m);
+        return hours+":"+minutes;
+    }
+
     public int getHours() {
-        int hours = (int)Math.round(hourHand / hourAngle);
-        return hours == 0 ? 12 : hours;
+        return hours;
     }
 
     public int getMinutes() {
-        return (int)Math.round(minHand / minuteAngle);
+        return minutes;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    private double getHourHandAngle() {
+        return ((hours % 12) * hourIncrement);
+    }
+
+    private double getMinuteHandAngle() {
+        return (minutes * minuteIncrement);
     }
 
     public boolean onTouchEvent(MotionEvent me) {
@@ -136,7 +147,7 @@ public class CustomTimePicker extends View {
             case (MotionEvent.ACTION_MOVE):
                 float newY = me.getHistoricalY(0);
                 float newX = me.getHistoricalX(0);
-                double newAngle = Math.atan((newY-midY)/(newX-midX));
+                double newAngle = Math.atan((newY-midY)/(newX-midX)) + Math.PI/2;
                 if (newX-midX < 0)
                     newAngle += Math.PI;
                 if (minGrabbed) updateMinuteHand(newAngle);
@@ -150,15 +161,39 @@ public class CustomTimePicker extends View {
     }
 
     private void updateMinuteHand(double newAngle) {
-        double minutes = (newAngle / minuteAngle);
-        minutes = Math.floor(minutes);
-        minHand = (float)(minutes * minuteAngle+Math.PI/2);
+        double minuteHandAngle = getMinuteHandAngle();
+        if (newAngle - minuteHandAngle > Math.PI) {
+            newAngle = (newAngle + Math.PI) % (2*Math.PI);
+            minuteHandAngle = (minuteHandAngle + Math.PI) % (2*Math.PI);
+        }
+
+        double increments = ((newAngle-minuteHandAngle) / minuteIncrement);
+        increments = Math.round(increments);
+        minutes = (minutes + (int)increments) % 60;
     }
 
     private void updateHourHand(double newAngle) {
-        double hours = (newAngle / hourAngle);
-        hours = Math.floor(hours);
-        hourHand = (float)(hours * hourAngle+Math.PI/2);
+        double hourHandAngle = getHourHandAngle();
+        if (newAngle - hourHandAngle > Math.PI) {
+            newAngle = (newAngle + Math.PI) % (2*Math.PI);
+            hourHandAngle = (hourHandAngle + Math.PI) % (2*Math.PI);
+        }
+
+        double increments = ((newAngle-hourHandAngle) / hourIncrement);
+        increments = Math.round(increments);
+        hours = (hours + (int)increments) % 24;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        updateSize();
+    }
+
+    private void updateSize() {
+        midX = getWidth() / 2f;
+        midY = getHeight() / 2f;
+        radius = Math.min(getWidth(), getHeight()) / 2f;
     }
 
     @Override
