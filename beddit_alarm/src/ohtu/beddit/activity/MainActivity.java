32cba85a3c33992328065d86b87549bf88ac7363
Joosa A S Kurvinen <joosakur@wop-5.cs.helsinki.fi>
2012-06-06 11:00:48 +0300

    notification AM PM

--- beddit_alarm/src/ohtu/beddit/activity/MainActivity.java
+++ beddit_alarm/src/ohtu/beddit/activity/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends Activity implements AlarmTimeChangedListener
         updateButtons();
         setClockHands();
 
-        boolean debugWeb = true;
+        boolean debugWeb = false;
         String token = PreferenceService.getSettingString(this, R.string.pref_key_userToken);
         if (token != null){
             Log.v("Token:", token);
--- beddit_alarm/src/ohtu/beddit/alarm/Notifications.java
+++ beddit_alarm/src/ohtu/beddit/alarm/Notifications.java
@@ -6,61 +6,54 @@ import android.app.PendingIntent;
 import android.content.ContentProviderOperation;
 import android.content.Context;
 import android.content.Intent;
+import android.text.format.DateFormat;
 import android.util.Log;
 import ohtu.beddit.R;
 import ohtu.beddit.activity.MainActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tahuomo
- * Date: 29.5.2012
- * Time: 13:23
- * To change this template use File | Settings | File Templates.
- */
 public class Notifications {
 
-    public static int setNotification(int ID, int startHours, int startMinutes, int endHours, int endMinutes ,Context context){
+    public static int setNotification(int ID, int startHours, int startMinutes, int endHours, int endMinutes, Context context){
         Notification notification = new Notification(R.drawable.alarm_notification,"",0);
         notification.flags = Notification.FLAG_NO_CLEAR;
 
-        String intervalBegin = timeAsString(startHours, startMinutes);
-        String intervalEnd = timeAsString(endHours, endMinutes);
-
-
-        String print  = "";
-        if (intervalBegin.equals(intervalEnd)){
-            print = intervalBegin;
-        } else {
-            print = intervalBegin + " - " + intervalEnd;
+        String print = timeAsString(startHours, startMinutes, context);
+        if (startHours != endHours || startMinutes != endMinutes){
+            print += " - " + timeAsString(endHours, endMinutes, context);;
         }
-
-        Intent intention = new Intent(context, MainActivity.class);
-        PendingIntent pendIntent = PendingIntent.getActivity(context, 0,intention,0);
-        notification.setLatestEventInfo(context, "Alarm set at ",print,pendIntent);
-        NotificationManager notificationman= (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationman.notify(ID, notification);
-
+        
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        notification.setLatestEventInfo(context, context.getString(R.string.notification_text),print,pendIntent);
+        NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(ID, notification);
 
         return ID;
     }
 
     public static void resetNotification(int ID, Context context){
-        NotificationManager notifications = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notifications.cancel(ID);
+        NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(ID);
     }
 
 
-    public static String timeAsString(int hour, int minute){
-        String hourString = "";
-        String minuteString = "";
-        if (hour < 10)
-            hourString += "0";
-        if (minute < 10)
-            minuteString += "0";
-        hourString += hour;
-        minuteString += minute;
-        return hourString+":"+minuteString;
+    public static String timeAsString(int hour, int minute, Context context){
+        Date date = new Date();
+        date.setHours(hour);
+        date.setMinutes(minute);
+
+        SimpleDateFormat dateFormat;
+        if(DateFormat.is24HourFormat(context)){
+            dateFormat = new SimpleDateFormat("HH:mm");
+        }
+        else{
+            dateFormat = new SimpleDateFormat("h:mm a");
+        }
+
+        return dateFormat.format(date);
     }
 }
