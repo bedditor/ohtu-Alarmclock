f328377914fbfb07ab0c2ff941cd5fcbc8362d27
Tero A Huomo <tero.huomo@helsinki.fi>
2012-06-26 11:02:06 +0300

    fixed the few automatic tests

--- beddit_alarm/tests/src/ohtu/beddit/activity/NotificationFactoryTest.java
+++ beddit_alarm/tests/src/ohtu/beddit/activity/NotificationFactoryTest.java
@@ -45,33 +45,4 @@ public class NotificationFactoryTest extends AndroidTestCase {
 
     }
 
-    /*  public void testTimeAsStringAMPM() throws Exception {
-    android.provider.Settings.System.putInt(getContext().getContentResolver(),
-            Settings.System.TIME_12_24,24);
-    Assert.assertEquals("15:50", ntf.timeAsString(15,50));
-
-}
-
-public void testTimeAsString() throws Exception {
-    android.provider.Settings.System.putInt(getContext().getContentResolver(),
-            Settings.System.TIME_12_24,12);
-
-    *//*Resources res = getContext().getResources();
-        // Change locale settings in the app.
-        DisplayMetrics dm = res.getDisplayMetrics();
-        android.content.res.Configuration conf = res.getConfiguration();
-        conf.locale = new Locale("en");
-        res.updateConfiguration(conf, dm);*//*
-
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) ;
-        intent.setClassName("com.android.settings", "com.android.settings.LanguageSettings");
-        getContext().startActivity(intent);
-
-        Thread.sleep(100000);
-
-        Assert.assertEquals("3:50 PM", ntf.timeAsString(15,50));
-    }*/
-
-
 }
